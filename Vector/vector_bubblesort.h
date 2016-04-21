9928fc3c44477a01118a93ce63ec2dd9f33e5ae4
RanchoCooper <1006959160@qq.com>
2016-04-22 00:21:20 +0800

    add some implement

---
+++ Vector/vector_bubblesort.h
@@ -0,0 +1,14 @@
+/*
+ * @author rancho cooper
+ * @email  rancho941110@gmail.com
+ * @date   2016-04-22 00:17:47
+ * @brief
+ * implement of class Vector
+ */
+#pragma once
+
+template <typename T>
+void Vector<T>::bubbleSort(Rank lo, Rank hi) {
+	//assert: 0 <= lo < hi <= size
+	while (!bubble(lo, hi--));
+}
