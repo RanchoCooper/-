260b5ea9324c0f6ad94445505eabe0eb0ac48733
RanchoCooper <1006959160@qq.com>
2016-04-26 22:45:26 +0800

    add annotation

--- Vector/vector_search.h
+++ Vector/vector_search.h
@@ -12,3 +12,9 @@ Rank Vector<T>::search(T const& e, Rank lo, Rank hi) const {
 	return (rand() % 2) ? //随机使用二分查找或者Fibonacci查找
 		binSearch(_elem, e, lo, hi) : finSearch(_elem, e, lo, hi);
 }
+
+//特殊情况: 目标元素不存在, 反过来, 存在多个目标元素
+//
+//语义约定:
+//即便失败, 也要给出新元素适当的插入位置(不大于e的最后一个元素)
+//
