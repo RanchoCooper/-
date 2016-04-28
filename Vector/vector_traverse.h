acf83a401e177b40e8ca84c71c450c6af57b61a6
RanchoCooper <1006959160@qq.com>
2016-04-28 20:28:53 +0800

    add bin_search version C

---
+++ Vector/vector_traverse.h
@@ -0,0 +1,15 @@
+/*
+ * @author rancho cooper
+ * @email  rancho941110@gmail.com
+ * @date   2016-04-28 14:14:09
+ * @brief
+ * implement of class Vector
+ */
+#pragma once
+#include <stdio.h>
+
+int main(int argc, char const *argv[]) {
+
+	getchar();
+	return 0;
+}
