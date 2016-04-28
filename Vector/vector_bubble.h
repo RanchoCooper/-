ab9e3be001ad122e937e3f18c34b9ce730d4c7c8
RanchoCooper <1006959160@qq.com>
2016-04-28 20:46:36 +0800

    add bubble sort

--- Vector/vector_bubble.h
+++ Vector/vector_bubble.h
@@ -10,10 +10,13 @@
 template <typename T>
 bool Vector<T>::bubble(Rank lo, Rank hi) {//一趟扫描交换
 	bool sorted = true;//整体有序标志
-	while (++lo < hi)
-		if (_elem[lo - 1] > _elem[lo]) {
+	while (++lo < hi) {//自左向右, 逐一检查各对相邻元素
+		if (_elem[lo] < _elem[lo - 1]) {
 			sorted = false;
-			swap(_elem[lo - 1], _elem[lo]);
+			swap(_elem[lo], _elem[lo - 1]);
 		}
-	return sorted;
+	}
+	return sorted;//返回有序标志
 }
+//虽然可以检测整体有序以提高速度
+//但不能检测出部分有序来跳过有序的区间
