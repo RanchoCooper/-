e451b5407103db32ed0e202030e7d297bc286eba
RanchoCooper <1006959160@qq.com>
2016-05-07 20:48:34 +0800

    add list_uniqufy.h

--- List/list_uniqufy.h
+++ List/list_uniqufy.h
@@ -5,5 +5,17 @@
  */
 #pragma once
 
-#include <iostream>
-
+template <typename T>
+int List<T>::uniquify() { //有序列表的唯一化
+	if (_size < 2) return 0;
+	int oldSize = _size;
+	ListNodePosi(T) p = first();
+	ListNodePosi(T) q = ;
+	while (trailer != (q = p->succ)) {
+		if (p->data != q->data)
+			p = q;
+		else
+			remove(q);
+	}
+	return oldSize - _size;
+}
